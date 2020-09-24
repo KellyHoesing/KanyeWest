e1fa165a25b21601c7f38ca4029e27a9371e1a17
Kelly <kelly.hoesing@gmail.com>
2020-09-24 10:39:49 -0500

    initial state

---
+++ KanyeWest/Kanye.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Kanye
+    {
+        public static void KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+            var client = new HttpClient();
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+        }
+        public static void RonQuote()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var client = new HttpClient();
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine($"Ron: {ronQuote}");
+        }
+    }
+}
---
+++ KanyeWest/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=10; i++)
+            {
+                if(i%2!=0)
+                {
+                    Kanye.KanyeQuote();
+                }
+                else
+                {
+                    Kanye.RonQuote();
+                }
+            }
+        }
+    }
+}
